87b231dd4c5d31b9cbb20b812e05b431d7ee23af
Deniz Jasarbasic <denizjasarbasic@Denizs-Mac-Pro.local>
2023-12-08 01:39:33 -0500

    initial

---
+++ components/NotionPageHeader.tsx
@@ -0,0 +1,118 @@
+import * as React from 'react'
+
+import * as types from 'notion-types'
+import { IoMoonSharp } from '@react-icons/all-files/io5/IoMoonSharp'
+import { IoSunnyOutline } from '@react-icons/all-files/io5/IoSunnyOutline'
+import cs from 'classnames'
+import { Breadcrumbs, Header, Search, useNotionContext } from 'react-notion-x'
+
+import { isSearchEnabled, navigationLinks, navigationStyle } from '@/lib/config'
+import { useDarkMode } from '@/lib/use-dark-mode'
+
+import styles from './styles.module.css'
+
+const ToggleThemeButton = () => {
+  const [hasMounted, setHasMounted] = React.useState(false)
+  const { isDarkMode, toggleDarkMode } = useDarkMode()
+
+  React.useEffect(() => {
+    setHasMounted(true)
+  }, [])
+
+  const onToggleTheme = React.useCallback(() => {
+    toggleDarkMode()
+  }, [toggleDarkMode])
+
+  return (
+    <div
+      className={cs('breadcrumb', 'button', !hasMounted && styles.hidden)}
+      onClick={onToggleTheme}
+    >
+      {hasMounted && isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
+    </div>
+  )
+}
+
+const ResumeButton = () => {
+  const [hasMounted, setHasMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  // URL to your resume in Google Drive for viewing
+  const resumeViewUrl = 'https://drive.google.com/file/d/1HetH2BFa2te5b4JcjJiSFEJ8ShMHh3r0/view?usp=sharing';
+
+  const viewResume = () => {
+    window.open(resumeViewUrl, '_blank');
+  };
+
+  return (
+    <div
+      className={cs('breadcrumb', 'button', !hasMounted && styles.hidden)}
+      onClick={viewResume}
+      role="button"
+      tabIndex={0}
+      onKeyPress={viewResume}
+    >
+      {hasMounted ? 'resume' : 'Loading...'}
+    </div>
+  );
+};
+
+
+
+export const NotionPageHeader: React.FC<{
+  block: types.CollectionViewPageBlock | types.PageBlock
+}> = ({ block }) => {
+  const { components, mapPageUrl } = useNotionContext()
+
+  if (navigationStyle === 'default') {
+    return <Header block={block} />
+  }
+
+  return (
+    <header className='notion-header'>
+      <div className='notion-nav-header'>
+        <b><Breadcrumbs block={block} rootOnly={true} /></b>
+
+        <div className='notion-nav-header-rhs breadcrumbs'>
+          {navigationLinks
+            ?.map((link, index) => {
+              if (!link.pageId && !link.url) {
+                return null
+              }
+
+              if (link.pageId) {
+                return (
+                  <components.PageLink
+                    href={mapPageUrl(link.pageId)}
+                    key={index}
+                    className={cs(styles.navLink, 'breadcrumb', 'button')}
+                  >
+                    {link.title}
+                  </components.PageLink>
+                )
+              } else {
+                return (
+                  <components.Link
+                    href={link.url}
+                    key={index}
+                    className={cs(styles.navLink, 'breadcrumb', 'button')}
+                  >
+                    {link.title}
+                  </components.Link>
+                )
+              }
+            })
+            .filter(Boolean)}
+
+          <ResumeButton />
+          <ToggleThemeButton />
+
+          {isSearchEnabled && <Search block={block} title={null} />}
+        </div>
+      </div>
+    </header>
+  )
+}
