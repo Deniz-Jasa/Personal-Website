00a6ae644c83acaf9eba170926e13c6d864715a7
Deniz Jasarbasic <denizjasarbasic@Denizs-Mac-Pro.local>
2023-12-18 13:04:49 -0500

    fixed open graph preview

--- pages/api/social-image.tsx
+++ pages/api/social-image.tsx
@@ -1,47 +1,24 @@
-import * as React from 'react'
-import { NextRequest } from 'next/server'
-
-import { ImageResponse } from '@vercel/og'
-
-import { api, apiHost, rootNotionPageId } from '@/lib/config'
-import { NotionPageInfo } from '@/lib/types'
+import * as React from 'react';
+import { NextRequest } from 'next/server';
+import { ImageResponse } from '@vercel/og';
 
 const interRegularFontP = fetch(
   new URL('../../public/fonts/Inter-Regular.ttf', import.meta.url)
-).then((res) => res.arrayBuffer())
+).then((res) => res.arrayBuffer());
 
 const interBoldFontP = fetch(
   new URL('../../public/fonts/Inter-SemiBold.ttf', import.meta.url)
-).then((res) => res.arrayBuffer())
+).then((res) => res.arrayBuffer());
 
 export const config = {
   runtime: 'experimental-edge'
-}
+};
 
 export default async function OGImage(req: NextRequest) {
-  const { searchParams } = new URL(req.url)
-  const pageId = searchParams.get('id') || rootNotionPageId
-  if (!pageId) {
-    return new Response('Invalid notion page id', { status: 400 })
-  }
-
-  const pageInfoRes = await fetch(`${apiHost}${api.getNotionPageInfo}`, {
-    method: 'POST',
-    body: JSON.stringify({ pageId }),
-    headers: {
-      'content-type': 'application/json'
-    }
-  })
-  if (!pageInfoRes.ok) {
-    return new Response(pageInfoRes.statusText, { status: pageInfoRes.status })
-  }
-  const pageInfo: NotionPageInfo = await pageInfoRes.json()
-  console.log(pageInfo)
-
   const [interRegularFont, interBoldFont] = await Promise.all([
     interRegularFontP,
     interBoldFontP
-  ])
+  ]);
 
   return new ImageResponse(
     (
@@ -56,34 +33,9 @@ export default async function OGImage(req: NextRequest) {
           alignItems: 'center',
           justifyContent: 'center',
           fontFamily: '"Inter", sans-serif',
-          color: 'black'
+          color: 'white'
         }}
       >
-        {pageInfo.image && (
-          <img
-            src={pageInfo.image}
-            style={{
-              position: 'absolute',
-              width: '100%',
-              height: '100%',
-              objectFit: 'cover'
-              // TODO: satori doesn't support background-size: cover and seems to
-              // have inconsistent support for filter + transform to get rid of the
-              // blurred edges. For now, we'll go without a blur filter on the
-              // background, but Satori is still very new, so hopefully we can re-add
-              // the blur soon.
-
-              // backgroundImage: pageInfo.image
-              //   ? `url(${pageInfo.image})`
-              //   : undefined,
-              // backgroundSize: '100% 100%'
-              // TODO: pageInfo.imageObjectPosition
-              // filter: 'blur(8px)'
-              // transform: 'scale(1.05)'
-            }}
-          />
-        )}
-
         <div
           style={{
             position: 'relative',
@@ -93,68 +45,22 @@ export default async function OGImage(req: NextRequest) {
             flexDirection: 'column',
             border: '16px solid rgba(0,0,0,0.3)',
             borderRadius: 8,
-            zIndex: '1'
+            zIndex: '1',
+            backgroundColor: '#fff',
+            justifyContent: 'center',
+            alignItems: 'center'
           }}
         >
           <div
             style={{
-              width: '100%',
-              height: '100%',
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'space-around',
-              backgroundColor: '#fff',
-              padding: 24,
-              alignItems: 'center',
-              textAlign: 'center'
+              fontSize: 70,
+              fontWeight: 700,
+              fontFamily: 'Inter'
             }}
           >
-            {pageInfo.detail && (
-              <div style={{ fontSize: 32, opacity: 0 }}>{pageInfo.detail}</div>
-            )}
-
-            {/* <div
-              style={{
-                fontSize: 70,
-                fontWeight: 700,
-                fontFamily: 'Inter'
-              }}
-            >
-              {pageInfo.title}
-            </div> */}
-
-            {pageInfo.detail && (
-              <div style={{ fontSize: 32, opacity: 0.6 }}>
-                {pageInfo.detail}
-              </div>
-            )}
+            Deniz Jasarbasic
           </div>
         </div>
-
-        {pageInfo.authorImage && (
-          <div
-            style={{
-              position: 'absolute',
-              top: 47,
-              left: 104,
-              height: 128,
-              width: 128,
-              display: 'flex',
-              borderRadius: '50%',
-              border: '4px solid #fff',
-              zIndex: '5'
-            }}
-          >
-            <img
-              src={pageInfo.authorImage}
-              style={{
-                width: '100%',
-                height: '100%'
-                // transform: 'scale(1.04)'
-              }}
-            />
-          </div>
-        )}
       </div>
     ),
     {
@@ -175,5 +81,5 @@ export default async function OGImage(req: NextRequest) {
         }
       ]
     }
-  )
+  );
 }
