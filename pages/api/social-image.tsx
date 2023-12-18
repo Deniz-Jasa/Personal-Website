1d9c37b1154f9b4fc7f49e0e12a8905032f22a9f
Deniz Jasarbasic <Denizj2828@gmail.com>
2023-12-18 14:51:10 -0500

    Update social-image.tsx

--- pages/api/social-image.tsx
+++ pages/api/social-image.tsx
@@ -1,52 +1,31 @@
-import * as React from 'react';
 import { ImageResponse } from '@vercel/og';
-
+ 
 export const config = {
-  runtime: 'experimental-edge'
+  runtime: 'experimental-edge',
 };
-
-export default async function OGImage() {
+ 
+export default function () {
   return new ImageResponse(
     (
       <div
         style={{
-          display: 'flex',
-          flexDirection: 'column',
-          backgroundColor: '#1F2027',
-          alignItems: 'center',
-          justifyContent: 'center',
+          fontSize: 40,
+          color: 'black',
+          background: 'white',
           width: '100%',
           height: '100%',
-          color: 'white'
+          padding: '50px 200px',
+          textAlign: 'center',
+          justifyContent: 'center',
+          alignItems: 'center',
         }}
       >
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            width: 900,
-            height: 465,
-            backgroundColor: '#fff',
-            borderRadius: 8,
-            border: '16px solid rgba(0,0,0,0.3)',
-          }}
-        >
-          <div
-            style={{
-              fontSize: 70,
-              fontWeight: 700
-            }}
-          >
-            Deniz Jasarbasic
-          </div>
-        </div>
+        Deniz Jasarbasic
       </div>
     ),
     {
       width: 1200,
-      height: 630
-    }
+      height: 630,
+    },
   );
 }
