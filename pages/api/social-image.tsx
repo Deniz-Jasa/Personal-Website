b439b8cd2acafa10f85215fd4fc4cc3ceae89081
Deniz Jasarbasic <Denizj2828@gmail.com>
2023-12-18 14:12:41 -0500

    Update social-image.tsx

--- pages/api/social-image.tsx
+++ pages/api/social-image.tsx
@@ -1,60 +1,42 @@
 import * as React from 'react';
 import { ImageResponse } from '@vercel/og';
 
-const interRegularFontP = fetch(
-  new URL('../../public/fonts/Inter-Regular.ttf', import.meta.url)
-).then((res) => res.arrayBuffer());
-
-const interBoldFontP = fetch(
-  new URL('../../public/fonts/Inter-SemiBold.ttf', import.meta.url)
-).then((res) => res.arrayBuffer());
-
 export const config = {
   runtime: 'experimental-edge'
 };
 
 export default async function OGImage() {
-  const [interRegularFont, interBoldFont] = await Promise.all([
-    interRegularFontP,
-    interBoldFontP
-  ]);
-
   return new ImageResponse(
     (
       <div
         style={{
-          position: 'relative',
-          width: '100%',
-          height: '100%',
           display: 'flex',
           flexDirection: 'column',
           backgroundColor: '#1F2027',
           alignItems: 'center',
           justifyContent: 'center',
-          fontFamily: '"Inter", sans-serif',
+          width: '100%',
+          height: '100%',
           color: 'white'
         }}
       >
         <div
           style={{
-            position: 'relative',
-            width: 900,
-            height: 465,
             display: 'flex',
             flexDirection: 'column',
-            border: '16px solid rgba(0,0,0,0.3)',
-            borderRadius: 8,
-            zIndex: '1',
-            backgroundColor: '#fff',
             justifyContent: 'center',
-            alignItems: 'center'
+            alignItems: 'center',
+            width: 900,
+            height: 465,
+            backgroundColor: '#fff',
+            borderRadius: 8,
+            border: '16px solid rgba(0,0,0,0.3)',
           }}
         >
           <div
             style={{
               fontSize: 70,
-              fontWeight: 700,
-              fontFamily: 'Inter'
+              fontWeight: 700
             }}
           >
             Deniz Jasarbasic
@@ -64,21 +46,7 @@ export default async function OGImage() {
     ),
     {
       width: 1200,
-      height: 630,
-      fonts: [
-        {
-          name: 'Inter',
-          data: interRegularFont,
-          style: 'normal',
-          weight: 400
-        },
-        {
-          name: 'Inter',
-          data: interBoldFont,
-          style: 'normal',
-          weight: 700
-        }
-      ]
+      height: 630
     }
   );
 }
