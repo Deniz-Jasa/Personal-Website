c4e078fb7a80184317967dee1dfb6ca1f260e245
Deniz Jasarbasic <denizj2828@gmail.comm>
2024-10-20 23:37:42 -0400

    fixed mobile view3

--- components/NotionPageHeader.tsx
+++ components/NotionPageHeader.tsx
@@ -136,7 +136,6 @@ export const NotionPageHeader: React.FC<{
             href="/"
             className={cs(styles.mobileNavLink, 'breadcrumb', 'button')}
             onClick={toggleMenu}
-            style={{ transitionDelay: `${(navigationLinks?.length || 0) * 0.1}s` }}
             rel="noopener noreferrer"
           >
             home
@@ -154,7 +153,6 @@ export const NotionPageHeader: React.FC<{
                   key={index}
                   className={cs(styles.mobileNavLink, 'breadcrumb', 'button')}
                   onClick={toggleMenu}
-                  style={{ transitionDelay: `${index * 0.1}s` }}
                 >
                   {link.title}
                 </LinkComponent>
@@ -165,7 +163,6 @@ export const NotionPageHeader: React.FC<{
             href={resumeViewUrl}
             className={cs(styles.mobileNavLink, 'breadcrumb', 'button')}
             onClick={toggleMenu}
-            style={{ transitionDelay: `${(navigationLinks?.length || 0) * 0.1}s` }}
             target="_blank"
             rel="noopener noreferrer"
           >
